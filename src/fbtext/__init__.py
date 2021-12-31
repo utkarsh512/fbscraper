2227ac49ef9835a7cc73da3dce4db76160b8895a
Utkarsh Patel <imutkarshpatel@gmail.com>
2021-12-31 17:58:18 +0530

    Delete src directory

--- src/fbtext/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Scraping texts from posts, comments and replies from any public Facebook page
-for creating new datasets for NLP research
-
-Author: Utkarsh Patel
-"""
-
-from scraper import Session
-
-__all__ = []
